e4708e9ef4d91a11b4452e46ba983e3c49f718ae
PedroManriquez <ptrmanriquez@gmail.com>
2019-08-15 10:30:18 -0400

    Usando el middleware de error para todas las request del API

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
 const cors = require('cors')
 const router = require('./routes')
+const error_middleware = require('./app/middlewares/error.middleware')
 
 const app = express()
 
 app.use(cors())
+app.use(error_middleware)
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
