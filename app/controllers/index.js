8ffb894e936bed7f40292547a9e5c887d46774a9
PedroManriquez <ptrmanriquez@gmail.com>
2019-08-16 12:58:58 -0400

    Eliminado archivo innecesario

--- app/controllers/index.js
+++
@@ -1,6 +0,0 @@
-const WeatherController = require('./Weather.controller')
-const cities = require('../data/cities').data
-
-let weather_controller = new WeatherController(cities)
-
-module.exports = weather_controller
